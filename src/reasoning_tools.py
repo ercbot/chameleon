06aab8b0d8977233171acc61be2ad5c03ab09482
Eric Botti <104158401+eric-botti@users.noreply.github.com>
2023-10-01 20:19:47 -0400

    experimental animal match tool

--- src/reasoning_tools.py
+++ src/reasoning_tools.py
@@ -24,6 +24,30 @@ def get_likely_animals(description: str) -> str:
     """Provides animals from a description"""
     return likely_animals_chain.run(animal_description=description)
 
+
+# Animal Match Tool
+ANIMAL_MATCH_PROMPT = """\
+Consider whether the following animal matches with the description provided. Provide your logic for reaching the conclusion. 
+ANIMAL:
+{animal}
+Description:
+{description}
+Your Thoughts:
+"""
+
+animal_match_template = PromptTemplate(
+    input_variables=['animal', 'description'],
+    template=ANIMAL_MATCH_PROMPT
+)
+
+animal_match_tool = LLMChain(llm=tool_llm, prompt=likely_animals_prompt)
+
+
+def does_animal_match_description(animal: str, description: str) -> str:
+    """Given an animal and a description, consider whether the animal matches that description"""
+    return animal_match_tool.run(animal=animal, description=description)
+
+
 tools = [
     Tool(
         name='get_likely_animals',
