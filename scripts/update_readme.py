c309f22d77c034307e844f1214cbb70342b235c1
Eric Botti <104158401+eric-botti@users.noreply.github.com>
2024-03-10 09:27:59 -0400

    streamlit version from requirements

--- scripts/update_readme.py
+++ scripts/update_readme.py
@@ -1,23 +1,35 @@
-import os
+"""
+Script to update the README file with the metadata required by Hugging Face Spaces.
+"""
 
+# Get Streamlit Version
+requirements_path = "requirements.txt"
+
+with open(requirements_path, 'r', encoding='utf-16') as requirements:
+    for line in requirements:
+        if line.strip().startswith("streamlit=="):
+            streamlit_version = line.strip().split('==')[1]
+            break
+
+# Update README
 readme_path = "README.md"
 
-hf_space_metadata="""
+hf_space_metadata = f"""
 ---
 title: Chameleon
 emoji: 🦎
 colorFrom: green
 colorTo: yellow
 sdk: streamlit
-sdk_version: 1.31.1
+sdk_version: {streamlit_version}
 app_file: src/app.py
 pinned: true
 ---
 """
 
 # Open the README file
-with open(readme_path, 'r') as original:
-    data = original.read()
+with open(readme_path, 'r') as original_readme:
+    data = original_readme.read()
 # Rewrite the README file, with metadata prepended
-with open(readme_path, 'w') as modified:
-    modified.write(hf_space_metadata + data)
+with open(readme_path, 'w', encoding="utf-16") as modified_readme:
+    modified_readme.write(hf_space_metadata + data)
